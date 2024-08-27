35940d9bbb1cd9231190acd13f3b3588672e08e4
Amidu99 <amidushamika@gmail.com>
2024-08-27 23:50:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import Users from './pages/Users';
 import './App.css';
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_USERS' });
+  }, [dispatch]);
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path='/' element={<Home />}></Route>                  
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/users' element={<Users />}></Route>                   
           </Route>
         </Routes>
       </BrowserRouter>
