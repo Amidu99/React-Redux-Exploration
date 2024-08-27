7e1669874f81ae79baac36bf0b8d34325dcc9696
Amidu99 <amidushamika@gmail.com>
2024-08-27 21:06:20 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>BlogAura</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />}></Route>                  
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
